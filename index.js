32df590e9ab8bdba5a4ee5a91392f0f3bb024af2
Mustakim <sycospike1@gmail.com>
2023-06-12 15:43:08 +0600

    added verifyJWT

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'});
+    }
+
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded ;
+        next();
+    } )
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -30,6 +49,13 @@ async function run() {
         const classCollection = client.db("danceClubdb").collection("classes");
         const selectedClassCollection = client.db("danceClubdb").collection("selectedClasses");
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+
+            res.send({token})
+        })
+
         // user related api
       app.get('/users', async(req, res) => {
         const result = await userCollection.find().toArray();
@@ -44,6 +70,20 @@ async function run() {
                 return res.send({message: 'user already exists'})
             }
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        // security
+        app.get('/users/admin/:email',verifyJWT, async(req, res) => {
+            const email = req.params.email;
+
+            if(req.decoded.email !== email){
+                res.send({admin: false})
+            }
+
+            const query = {email: email};
+            const user = await userCollection.findOne(query);
+            const result = {admin: user?.role === 'admin'};
             res.send(result);
         })
 
@@ -67,11 +107,17 @@ async function run() {
 
         // selected classes apis
 
-        app.get('/selectedClasses', async(req, res) => {
+        app.get('/selectedClasses',verifyJWT, async(req, res) => {
             const email = req.query.email;
             if(!email){
                 res.send([]);
             }
+
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail){
+                return res.status(403).send({error: true, message: 'forbidden access'})
+            }
+
             const query = {email: email};
             const result = await selectedClassCollection.find(query).toArray();
             res.send(result);
