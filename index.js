8bdc5c070eae457e54d268c7b19b29dee3a8d2d4
Mustakim <sycospike1@gmail.com>
2023-06-12 20:44:39 +0600

    added verifyAdmin

--- index.js
+++ index.js
@@ -56,8 +56,19 @@ async function run() {
             res.send({token})
         })
 
+        // warning: use verifyJwt before using verifyAdmin
+        const verifyAdmin = async(req, res, next)=>{
+            const email = req.decoded.email;
+            const query = {email: email}
+            const user = await userCollection.findOne(query);
+            if(user?.role !== 'admin'){
+                return res.status(403).send({error: true, message: 'forbidden message'})
+            }
+            next();
+        }
+
         // user related api
-      app.get('/users', async(req, res) => {
+      app.get('/users',verifyJWT,verifyAdmin, async(req, res) => {
         const result = await userCollection.find().toArray();
         res.send(result);
       })
