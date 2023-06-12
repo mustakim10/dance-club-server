98a36572d0811b56139f6eb9ff3fbfc2908ffd65
Mustakim <sycospike1@gmail.com>
2023-06-12 09:57:16 +0600

    added delete from selected classes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qroq892.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -50,6 +50,13 @@ async function run() {
             const selected = req.body ;
             console.log(selected);
             const result = await selectedClassCollection.insertOne(selected);
+            res.send(result);
+        })
+
+        app.delete('/selectedClasses/:id', async(req, res) => {
+            const id = req.params.id ;
+            const query = { _id: new ObjectId(id)};
+            const result = await selectedClassCollection.deleteOne(query);
             res.send(result);
         })
 
